b1a95d1605096e9655cfd7a870b29c210d1afe24
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-15 13:20:29 -0200

    Segunda Fase - Migration - Seeders, Models, Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,34 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/*$factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});*/
+$factory->define(App\UserAccount::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
+$factory->define(App\Account::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $this->call(UsersTableSeeder::class);
+        $this->call(AccountsTableSeeder::class);
+        $this->call(UserAccountsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
     }
 }
