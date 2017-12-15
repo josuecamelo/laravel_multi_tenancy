3d063ee071bb5b23ca02aaa5482f4fe56d22d562
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-15 14:23:07 -0200

    Capturando queries executadas no banco

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        DB::listen(function(QueryExecuted $query){
+            \Log::info($query->sql);
+            \Log::info($query->bindings);
+        });
     }
 
     /**
