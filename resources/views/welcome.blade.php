6d4e9a8ccb2c36c44686a934df586b9d149aab3f
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-18 14:52:15 -0200

    Ajustes finais

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,8 +71,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login', ['account' => \Request::route('account')]) }}">Login</a>
-                        <a href="{{ route('register', ['account' => \Request::route('account')]) }}">Register</a>
+                        <a href="{{ routeTenant('login') }}">Login</a>
+                        <a href="{{ routeTenant('register') }}">Register</a>
                     @endauth
                 </div>
             @endif
